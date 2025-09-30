770416d470f4d091a9ae92e4d6d649132d8efee7
Linggar <linggarsulisyanti@gmail.com>
2025-09-30 22:01:23 +0700

    menambahkan file Array.java di folder array&collection

---
+++ array&collection/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        // Deklarasi array dengan panjang tetap
+        String[] nama = {"Ani", "Budi", "Citra"};
+
+        // Menampilkan isi array
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println("Nama ke-" + i + ": " + nama[i]);
+        }
+    }
+}
